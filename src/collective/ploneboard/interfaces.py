07144854eff97cf0e0d4dd1ee02c26b95893cf35
Ammy2 <aman2arora2@gmail.com>
2014-05-20 22:54:08 +0530

    Categories added

--- src/collective/ploneboard/interfaces.py
+++ src/collective/ploneboard/interfaces.py
@@ -1,13 +1,15 @@
 # -*- coding: UTF-8 -*-
 from plone.app.textfield import RichText
 from plone.directives import form
-
+from zope import schema
 from collective.ploneboard import _
 
 
 class IMessageboard(form.Schema):
-    """
-    """
+	category=schema.Text(
+		title=_(u"Categories"),
+		required=False
+		)
 
 
 class ITopic(form.Schema):
