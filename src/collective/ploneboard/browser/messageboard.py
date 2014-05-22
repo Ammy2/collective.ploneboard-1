6b8f6bc25e1197c152c192d0b547e010f30d9fde
Ammy2 <aman2arora2@gmail.com>
2014-05-22 14:35:44 +0530

    Refactored to less complexity O(n) from O(n^2)

--- src/collective/ploneboard/browser/messageboard.py
+++ src/collective/ploneboard/browser/messageboard.py
@@ -8,11 +8,34 @@ class MessageboardView(BrowserView):
     template = ViewPageTemplateFile('messageboard.pt')
 
     def __call__(self):
-	context = self.context.aq_inner
-	#print context.category
-	context.cats = context.category.split('\r\n')
-	#print context.cats
+        context = self.context.aq_inner
+        #print context.category
+        context.cats = context.category.split('\r\n')
+        #print context.cats
         return self.template()
+
+    def categories(self):						# Returns topics grouped by categories
+    	categ={}
+        for topic_id in self.context.objectIds():
+            topic = self.context[topic_id]
+            conversations = getMultiAdapter(
+                (topic, self.request),
+                name="view"
+            ).conversations()
+            for each_category in topic.category:
+            	if each_category not in categ:
+                    key = each_category
+                    categ[key] = []				# New category arrived
+                    categ[key].append({ 'title': topic.title,'category': topic.category, 'url': topic.absolute_url(), 'conversations': conversations})
+                else:
+                    key = each_category
+                    categ[key].append({			# Topic added to that category
+		                'title': topic.title,
+						'category': topic.category,
+		                'url': topic.absolute_url(),
+		                'conversations': conversations,
+		            })
+        return categ
 
     def topics(self):
         topics = []
@@ -24,7 +47,7 @@ class MessageboardView(BrowserView):
             ).conversations()
             topics.append({
                 'title': topic.title,
-		'category': topic.category,
+				'category': topic.category,
                 'url': topic.absolute_url(),
                 'conversations': conversations,
             })
